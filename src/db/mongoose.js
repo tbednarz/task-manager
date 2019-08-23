3c8d8b560625dc94e63709993be73d236bdb5cff
MasterTres <tresbednarz@gmail.com>
2019-08-23 14:42:13 -0400

    added mongoose models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 
@@ -7,34 +8,72 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const Task = mongoose.model('Tasks', {
+//Create a task model 
+const task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true
     },
     completed: {
         type: Boolean
     }
 })
-
-const task= new Task({description:'Fuck the snow', completed: false})
-task.save().then((task) =>{
-    console.log(task)
-}).catch((e) => {
-    console.log("ERROR: ", e)
-})
-
+//Create a User model
 const User = mongoose.model('User', {
+    //name takes type string, is required and trims spaces
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+
     },
+    //email takes type string, is required, trims and to lowercase
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new error('Email is invalid')
+            }
+        }
+    },
+    //age takes type number, defaults to 0, if input age is less that
+    // 0, error is thrown
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new error('Age must be a positive number')
+            }
+        }
+
     }
 })
 
-const me = new User({name:'Tres', age: 27})
-me.save().then((me) =>{
+//create new user filling in user model data
+const me = new User({
+    name: "   Tres",
+    email: "MYEMAIL@BOINK.io",
+    age: 27
+
+
+})
+//save new user and log to console
+me.save().then((me) => {
     console.log(me)
+    //throw error if went wrong
 }).catch((e) => {
     console.log('ERROR', e)
 })
+// const task = new task({
+//     description: 'Fuck the snow',
+//     completed: false
+// })
+// task.save().then((task) => {
+//     console.log(task)
+// }).catch((e) => {
+//     console.log("ERROR: ", e)
+// })
